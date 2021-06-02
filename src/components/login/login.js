cb9098b472698e83eb86fde3e2fe6fa94b0bdbbc
Mahmoudgawish22 <[mahmoudgawish11@gmail.com]>
2021-06-02 16:54:58 +0200

    nn

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -53,7 +53,7 @@ function Login(props) {
   if (!user?.result?.name) {
     return (
         <div id='login' style={{backgroundImage: `url(${logo})`}}>
-            <h1 style={{color: 'white', fontFamily:  'Tajawal'}}>
+            <h1 style={{color: 'white', fontFamily: "'Barlow Condensed'"}}>
                 Welcome To Omar Timo C-Panel, Please Log in to Continue!
             </h1>
             <form onSubmit={handleSubmit} style={{textAlign: 'left',padding: '30px', borderRadius: '5px', backgroundColor: 'rgba(255, 255, 255, 0.8)'}}>
